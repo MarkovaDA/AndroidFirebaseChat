e2a5ce8df0fc763f74d6f01000cc49cfea7ca0e3
markova <markova@reksoft.ru>
2018-12-28 18:57:47 +0300

    starting application

---
+++ app/src/main/java/com/markova/darya/simplechat/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.markova.darya.simplechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/markova/darya/simplechat/MainActivity.java
@@ -0,0 +1,62 @@
+package com.markova.darya.simplechat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    public int SIGN_IN_REQUEST_CODE = 10;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() == null) {
+            startActivityForResult(
+                    new Intent(this, LoginActivity.class),
+                    SIGN_IN_REQUEST_CODE
+            );
+        } else {
+            Toast.makeText(this,
+                    getResources().getString(R.string.greeting)
+                            .concat(firebaseAuth
+                                    .getCurrentUser()
+                                    .getDisplayName()),
+                    Toast.LENGTH_LONG)
+                    .show();
+
+            displayChatMessages();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        /*if(requestCode == SIGN_IN_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        getResources().getString(R.string.success_auth),
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                        getResources().getString(R.string.failure_auth),
+                        Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+        }*/
+    }
+
+    private void displayChatMessages() {
+
+    }
+}
