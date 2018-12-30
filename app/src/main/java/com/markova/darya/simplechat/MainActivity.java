df394deef53fc97dd10b4cde08062094c4243366
darya-markova <darya.towarddreams@gmail.com>
2018-12-31 00:52:10 +0300

    main menu modification

--- app/src/main/java/com/markova/darya/simplechat/MainActivity.java
+++ app/src/main/java/com/markova/darya/simplechat/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.support.v7.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -22,8 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.sign_out_menu, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.getItem(0).setTitle(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        return super.onPrepareOptionsMenu(menu);
     }
 
     private void displayChatMessages() {
